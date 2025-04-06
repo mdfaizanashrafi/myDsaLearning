492b93682481d655dbdda348bdcc6fbf0e244b03
mdfaizanashrafi <mdfaizanashrafi13032001@gmail.com>
2025-04-06 15:07:40 +0530

    more questions

--- src/array_ex.py
+++ src/array_ex.py
@@ -1,3 +1,6 @@
+#EASY LEVEL QUESTIONS:
+#=====================================================
+
 #reverse an array:
 
 #method 1:
@@ -168,8 +171,84 @@ def twoo_equal_array_sort(array1,array2):
 
 #Find the second largest element in an array:
 
+def second_lar(nums):
+    max=0
+    max2=0
+    for num in nums:
+        if num>max:
+            max2=max
+            max=num
+    return max2
 
+#method 2:
+def second_largest(nums):
+    return sorted(nums)[-2]
+
+#method:3
+def second_larg(nums):
+    if len(nums)<2:
+        return "Array should have at least 2 elements"
+    
+    max_val= float('-inf')
+    max2_val=float('-inf')
+
+    for num in nums:
+        if num>max_val:
+            max2_val=max_val
+            max_val=num
+        elif num>max2_val:
+            max2_val=num
+    
+    if max2_val==float('-inf'):
+        return "No valid second largest number"
+
+    return max2_val
             
+
+#method 4:
+def second_large(nums):
+    hash_set= set(nums)
+    if len(nums)<2:
+        return "Array should have at least 2 elements"
+    return hash_set.sorted([-2])
+
+#find all elements that appear more than once in an array:
+def find_duplicates(array):
+    hash_set=set(array)
+    for num in hash_set:
+        count =0
+        for i in range(len(array)):
+            if num==array[i]:
+                count+=1
+        print(f"Total number of element {num} in array is {count}")
+    
+#method 2:
+def find_duplicates_using_dict(array):
+    freq={}
+    for num in array:
+        freq[num]=freq.get(num,0)+1
+    
+    duplicates=[num for num,count in freq.items() if count>1]
+
+    return duplicates
+    
+
+#Merge two sorted arrays:
+
+def merge_arrays(arr1,arr2):
+    arr3=arr1+arr2
+    return sorted(arr3)
+
+
+#=================================================================
+#MEDIUM LEVEL QUESTIONS:
+#=================================================================
+
+#Move all zeros to the end of the array
+
+
+
+
         
 
 
