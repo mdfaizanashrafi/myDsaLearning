58ae282f2645ea24f362e5fcb21c2289c9222cdd
mdfaizanashrafi <mdfaizanashrafi13032001@gmail.com>
2025-06-05 22:34:04 +0530

    more

--- leet_code.py
+++ leet_code.py
@@ -523,3 +523,56 @@ class Solution:
                 right -= 1
         return water_trapped
                      
+
+#===================================================
+# 3 sum: leetcode: 
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res=[]
+        for i in range(len(nums)):
+            if i>0 and nums[i]==nums[i-1]:
+                continue
+            left, right= i+1, len(nums)-1
+            while left<right:
+                total= nums[i]+nums[left]+nums[right]
+                if total ==0:
+                    res.append([nums[i],nums[left],nums[right]])
+                    left +=1
+                    right -=1
+                    while left< right and nums[left] == nums[left]-1:
+                        left +=1
+                    while left< right and nums[right] == nums[right+1]:
+                        right -=1
+                elif total<0:
+                    left+=1
+                else:
+                    right -=1
+
+        return res
+
+
+#======================================================
+
+#valid oalindrome #leetcode 125
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        left, right= 0, len(s)-1
+        while left<right:
+            while left<right and not s[left].isalnum():
+                left+=1
+            while left<right and not s[right].isalnum():
+                right -=1
+            
+            if s[left].lower() != s[right].lower():
+                return False
+            
+            left +=1
+            right -=1
+        
+        return True
+
+
+#===========================================
