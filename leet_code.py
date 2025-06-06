e281e786b097198428d6ca31f5c9ecb8da0b6e3f
mdfaizanashrafi <mdfaizanashrafi13032001@gmail.com>
2025-06-06 17:49:12 +0530

    koko bananas

--- leet_code.py
+++ leet_code.py
@@ -629,3 +629,24 @@ class Solution:
 
 
 #=================================================
+
+#875: koko eating bananaas
+
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        low=1
+        high = max(piles)
+        while low< high:
+            mid= (low+high)//2
+            total_hours = 0
+            for pile in piles:
+                total_hours += math.ceil(pile/mid)
+
+            if total_hours <= h:
+                high = mid
+            else:
+                low= mid+1
+
+        return low
+
+#==============================================
