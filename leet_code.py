b86ae4e82c2805f0a34a388b1ae428a351e8c4da
mdfaizanashrafi <mdfaizanashrafi13032001@gmail.com>
2025-06-07 00:21:31 +0530

    Q 33

--- leet_code.py
+++ leet_code.py
@@ -667,3 +667,26 @@ class Solution:
         return nums[left]
 
 #==================================================
+#search in rotated sorted array #leetcode:33
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right=0, len(nums)-1
+        while left<=right:
+            mid = (left+right)//2
+            if nums[mid]==target:
+                return mid
+            if nums[left]<=nums[mid]:
+                if nums[left]<=target<nums[mid]:
+                    right=mid-1
+                else:
+                    left= mid+1
+            else:
+                if nums[mid]<target<=nums[right]:
+                    left=mid+1
+                else:
+                    right= mid-1
+            
+        return -1
+
+#========================================================
