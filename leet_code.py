2e7d07627d3ed1c13b5343f0025ea0f560d162c1
mdfaizanashrafi <mdfaizanashrafi13032001@gmail.com>
2025-06-13 21:00:04 +0530

    lru cache

--- leet_code.py
+++ leet_code.py
@@ -1125,3 +1125,58 @@ class Solution:
 
         
         return slow
+    
+#========================================================================
+
+#146: LRU Cache
+
+class ListNode:
+    def __init__(self, key=0, val=0):
+        self.key = key
+        self.val = val
+        self.prev = None
+        self.next = None
+        
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.cap = capacity
+        self.cache = dict()
+        self.head = ListNode()
+        self.tail = ListNode()
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def _add(self, node: ListNode):
+        node.prev = self.head
+        node.next = self.head.next
+        self.head.next.prev = node
+        self.head.next = node
+
+    def _remove(self, node: ListNode):
+        prev_node = node.prev
+        next_node = node.next
+        prev_node.next = next_node
+        next_node.prev = prev_node
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        node = self.cache[key]
+        self._remove(node)
+        self._add(node)
+        return node.val
+
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self._remove(self.cache[key])
+
+        elif len(self.cache) >= self.cap:
+            lru = self.tail.prev
+            self._remove(lru)
+            del self.cache[lru.key]
+
+        new_node = ListNode(key, value)
+        self._add(new_node)
+        self.cache[key] = new_node
+
