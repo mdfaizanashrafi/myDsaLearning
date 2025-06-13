27c1f18d93ed73c33efd8e23374538a5a0016397
mdfaizanashrafi <mdfaizanashrafi13032001@gmail.com>
2025-06-13 19:40:47 +0530

    more question on linked list

--- src/linked_lists.py
+++ src/linked_lists.py
@@ -59,3 +59,112 @@ class Solution:
             tail=tail.next
         tail.next=list1 or list2
         return dummy.next
+
+#===========================================================
+#141. Linked List Cycle
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast :
+                return True
+
+        return False
+    
+#===========================================================
+
+#143. Reorder List
+
+class Solution :   
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        if not head or not head.next:
+            return
+        
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        prev, curr = None, slow
+
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+
+        first, second = head, prev
+
+        while second.next:
+            temp = first.next
+            first.next = second
+            first = temp
+
+            temp = second.next
+            second.next = first
+            second = temp
+
+
+#==============================================================================
+#Copy list with random pointer  #138
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        if not head:
+            return None
+
+        curr = head
+        while curr:
+            clone = Node(curr.val)
+            clone.next = curr.next
+            curr.next = clone
+            curr = clone.next
+
+        curr = head
+        while curr:
+            if curr.random:
+                curr.next.random = curr.random.next
+            curr = curr.next.next
+            
+        cloned_head = head.next
+        curr = head
+        while curr and curr.next:
+            clone = curr.next
+            curr = clone.next
+
+            if clone.next:
+                clone.next = clone.next.next
+            
+        return cloned_head
+    
+#==============================================================================
+
+#19. Remove Nth Node From End of List
+
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        dummy = ListNode(0)
+        dummy.next = head
+        fast = slow = dummy
+
+        for _ in range(n+1):
+            fast = fast.next
+
+        while fast:
+            fast = fast.next
+            slow = slow.next
+
+        slow.next = slow.next.next
+
+        return dummy.next
+    
+#==========================================================================
