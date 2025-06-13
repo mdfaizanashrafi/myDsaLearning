842deeb6ea6eb76f62f7e6712ef63bed86338205
mdfaizanashrafi <mdfaizanashrafi13032001@gmail.com>
2025-06-13 20:05:39 +0530

    find duplicate using LL

--- leet_code.py
+++ leet_code.py
@@ -1078,4 +1078,50 @@ class Solution:
         return dummy.next
 
 #==========================================================================
+# 2. Add Two Numbers
 
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(0)
+        curr = dummy
+        carry = 0
+
+
+        while l1 or l2 or carry:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+
+            total = val1 + val2 + carry
+            carry = total//10
+            digit = total % 10
+            curr.next = ListNode(digit)
+
+            curr = curr.next
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+
+        return dummy.next
+
+#=================================================================================
+#287. Find the Duplicate Number
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        slow = nums[0]
+        fast = nums[0]
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+
+        
+        slow = nums[0]
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[fast]
+
+        
+        return slow
