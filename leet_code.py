5036c26b2a5698ba58e061e87af3a912c1f5b3b2
mdfaizanashrafi <mdfaizanashrafi13032001@gmail.com>
2025-06-15 00:57:59 +0530

    same tree

--- leet_code.py
+++ leet_code.py
@@ -1334,7 +1334,9 @@ class Solution:
         
         dfs(root)
         return self.diameter
-    
+ 
+ #=========================================================================
+
 #110: Balannced Binary tree
 
 class Solution:
@@ -1356,4 +1358,19 @@ class Solution:
 
         return dfs(root) != -1
     
-#
+#========================================================================
+
+#100. Same Binary Tree
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
+
+        if not p or not q:
+            return False
+
+        if p.val != q.val:
+            return False
+        
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
