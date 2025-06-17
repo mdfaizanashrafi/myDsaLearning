42f1647ca4f60a37e062d392d30e3b6b3f0e180a
mdfaizanashrafi <mdfaizanashrafi13032001@gmail.com>
2025-06-17 11:25:43 +0530

    more

--- src/heap_priority_q.py
+++ src/heap_priority_q.py
@@ -24,4 +24,48 @@ class KthLargest:
 
 #1046. Last Stone Weight
 
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        max_heap = [-stone for stone in stones]
+        heapq.heapify(max_heap)
+        while len(max_heap)>1:
+            y = -heapq.heappop(max_heap)
+            x= -heapq.heappop(max_heap)
+            if y != x:
+                new_stone = y-x
+                heapq.heappush(max_heap, -new_stone)
+            
+        return -max_heap[0] if max_heap else 0
+
+#===========================================================================================
+
+#973. K Closest Points to Origin
+
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        max_heap = []
+        for x,y in points:
+            dist = -(x*x + y*y)
+            heapq.heappush(max_heap, (dist, [x,y]))
+            if len(max_heap) > k:
+                heapq.heappop(max_heap)
+
+        return [point for (_, point) in max_heap]
+
+#================================================================================
+
+#215. Kth Largest Element in an Array
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        min_heap = []
+        for num in nums:
+            heapq.heappush(min_heap, num)
+            if len(min_heap) > k:
+                heapq.heappop(min_heap)
+        return min_heap[0]
+
+#============================================================================
+
+#621. Task Scheduler
 
