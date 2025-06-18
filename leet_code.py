1d07e6f5cb782936e08e78c25751e90d5b9468dc
mdfaizanashrafi <mdfaizanashrafi13032001@gmail.com>
2025-06-18 22:35:10 +0530

    oranges

--- leet_code.py
+++ leet_code.py
@@ -2294,3 +2294,39 @@ class Solution:
                     grid[nr][nc] = grid[r][c]+1
                     queue.append((nr,nc))
 
+#======================================================================================
+
+#994. Rotting Oranges
+
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        if not grid or not grid[0]:
+            return 0
+
+        rows, cols = len(grid), len(grid[0])
+        queue = deque()
+        fresh = 0
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c]==1:
+                    fresh += 1
+                elif grid[r][c] == 2:
+                    queue.append((r,c))
+
+        directions=[(-1,0),(1,0),(0,-1),(0,1)]
+        time = 0
+
+        while queue and fresh > 0:
+            for _ in range(len(queue)):
+                r,c = queue.popleft()
+                for dr,dc in directions:
+                    nr,nc = r+dr, c+dc
+
+                    if 0<=nr<rows and 0<=nc<cols and grid[nr][nc]==1:
+                        grid[nr][nc]=2
+
+                        fresh -= 1
+                        queue.append((nr,nc))
+            time += 1
+        return time if fresh == 0 else -1
+
