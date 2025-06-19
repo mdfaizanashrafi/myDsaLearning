cdcabca454cb86198af48b914b7dd562a923fd1c
mdfaizanashrafi <mdfaizanashrafi13032001@gmail.com>
2025-06-19 20:15:06 +0530

    more que

--- leet_code.py
+++ leet_code.py
@@ -2441,3 +2441,28 @@ class Solution:
 
         return count == numCourses
     
+#========================================================================================
+
+#210. Course Schedule II
+
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        graph = [[] for _ in range(numCourses)]
+        in_degree = [0]*numCourses
+
+        for dest, src in prerequisites:
+            graph[src].append(dest)
+            in_degree[dest] += 1
+        queue = deque([i for i in range(numCourses) if in_degree[i] ==0])
+
+        result = []
+        while queue:
+            curr = queue.popleft()
+            result.append(curr)
+            for neighbor in graph[curr]:
+                in_degree[neighbor] -= 1
+                if in_degree[neighbor] == 0:
+                    queue.append(neighbor)
+            
+        return result if len(result) == numCourses else []
+
