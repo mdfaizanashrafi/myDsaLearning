814403b9b7e2a703bc1641c7fc01ba21aaf13825
mdfaizanashrafi <mdfaizanashrafi13032001@gmail.com>
2025-06-21 13:20:27 +0530

    greedy started

--- leet_code.py
+++ leet_code.py
@@ -3663,3 +3663,13 @@ class Solution:
 
 #53. Maximum Subarray
 
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        max_current = max_global = nums[0]
+
+        for num in nums[1:]:
+            max_current = max(num, max_current + num)
+            max_global = max(max_global, max_current)
+
+        return max_global
+
--- src/greddyy.py
+++ src/greddyy.py
@@ -217,3 +217,17 @@ def fractionalKnapsack(items, capacity):
 - **Optimization in finance** (portfolio selection)
 - **Resource allocation**
 """
+#=============================================================================================
+
+#53. Maximum Subarray
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        max_current = max_global = nums[0]
+
+        for num in nums[1:]:
+            max_current = max(num, max_current + num)
+            max_global = max(max_global, max_current)
+
+        return max_global
+
